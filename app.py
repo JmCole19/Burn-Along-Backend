63be63a22f10db8d4e23fb8df01f85168af2e101
JmCole19 <jayemcee121@gmail.com>
2021-06-07 14:02:07 -0400

    Data from api is being printed into console, preparing to display data

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import requests
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///list.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Item(db.Model):
@@ -17,9 +19,35 @@ class Item(db.Model):
 def __repr__(self):
 	return '<Item %r>' % self.id
 
+def get_data():
+    data_url = ('http://code.burnalong.com/items')
+    try:
+        _r = requests.get(
+            data_url,
+            # headers=_headers
+        )
+        return _r.json()
+    except Exception as ee:
+        print('Could not get data: {}'.format(ee))
+        return None
+
+def main():
+    db.create_all()
+    get_data()
+# print (_r.json())
+# print (_data)
+
+# def add_to_db(_data):
+#     if _data:
+#         try:
+
+
+
 @app.route("/")
-def hello_world():
+def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    with app.app_context():
+        main()
+        app.run(debug=True)
